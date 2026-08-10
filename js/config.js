d2799bae62b4881c059aa8855844eee7b71400d0
fahmikip <fahmikip2026@gmail.com>
2026-08-10 14:34:18 +0700

    Switch production GAS deployment

--- js/config.js
+++ js/config.js
@@ -1,2 +1,2 @@
 /* Edit only this file when deploying to a different environment. */
-const CONFIG=Object.freeze({API_URL:"https://script.google.com/macros/s/AKfycbwltuFI950AY3E0DwuVhF0ErKM6i2-RwMnD3saEejmEbMvb8z68xku6AjctxeKYxLkC/exec",BASE_PATH:"/pemilih-muda/",APP_NAME:"Pemilih Muda",APP_VERSION:"1.5.0",REQUEST_TIMEOUT_MS:15000,DEBUG:false});
+const CONFIG=Object.freeze({API_URL:"https://script.google.com/macros/s/AKfycbyFqFE4XIci-q_LiWwXz9rPOT9ZqzVzk-7CmsGJ6jRp_gJqto-z0CdSqwGPxixw19ZY/exec",BASE_PATH:"/pemilih-muda/",APP_NAME:"Pemilih Muda",APP_VERSION:"1.5.0",REQUEST_TIMEOUT_MS:15000,DEBUG:false});
