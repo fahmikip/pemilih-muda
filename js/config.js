2338c5b6ff1446545aa08d426c4886b6c3200f6d
fahmikip <fahmikip2026@gmail.com>
2026-08-10 12:22:28 +0700

    Update production GAS endpoint

--- js/config.js
+++ js/config.js
@@ -1,2 +1,2 @@
 /* Edit only this file when deploying to a different environment. */
-const CONFIG=Object.freeze({API_URL:"https://script.google.com/macros/s/AKfycbxAsf0ng2Ran3Xbv9Ic7I3IAt77m26UtKfqOLWiPBcJ-HLlywi5qeDyy2PbXeae7XQf/exec",BASE_PATH:"/pemilih-muda/",APP_NAME:"Pemilih Muda",APP_VERSION:"1.3.0",REQUEST_TIMEOUT_MS:15000,DEBUG:false});
+const CONFIG=Object.freeze({API_URL:"https://script.google.com/macros/s/AKfycbw08jzqizbp54m9fN00TncNDzCGVwjEYQcVGwlCPW3ApmR-5B8uS6NYooX81HFLGGr2/exec",BASE_PATH:"/pemilih-muda/",APP_NAME:"Pemilih Muda",APP_VERSION:"1.3.0",REQUEST_TIMEOUT_MS:15000,DEBUG:false});
